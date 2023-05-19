046eb03e316d41713db1a86ba13eb20822931874
dhclarke99 <dhclarke99@gmail.com>
2023-05-18 20:07:16 -0400

    added removeFriend function

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,6 +49,16 @@ module.exports = {
   },
 
   removeFriend(req, res) {
+    User.findOneAndUpdate(
+      {_id: req.params.userId},
+      { $pull: { friends: { friendId: req.params.friendId } } },
+      { runValidators: true, new: true }
+      )
+      .then((user) =>
+    !user
+    ? res.status(404).json({message: 'No user with that ID'})
+    : res.json(user)
+    )
 
   }
 };
